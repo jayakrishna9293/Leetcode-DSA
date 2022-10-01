99792a654283c5f142c03a9fd1977df8c879bb3c
jayakrishna9293 <55653899+jayakrishna9293@users.noreply.github.com>
2022-10-01 11:45:09 +0530

    Add files via upload

---
+++ excel_column_number.java
@@ -0,0 +1,13 @@
+public class excel_column_number {
+    public int titleToNumber(String columnTitle) {
+        int ans = 0;
+
+        for (int i = 0; i < columnTitle.length(); i++) {
+            ans *= 26;
+            char c = columnTitle.charAt(i);
+            int n = (int) (c - 'A' + 1);
+            ans += n;
+        }
+        return ans;
+    }
+}
