4a8c91bbf420dbdf373230ad9e839223ff03171f
jayakrishna9293 <55653899+jayakrishna9293@users.noreply.github.com>
2022-10-10 09:43:36 +0530

    Add files via upload

---
+++ min_cost_climbing_stairs.java
@@ -0,0 +1,10 @@
+package math
+
+class min_cost_climbing_stairs {
+     public int minCostClimbingStairs(int[] cost)
+        {
+            for(int i=2;i<cost.length;i++)
+                cost[i] = Math.min(cost[i-1],cost[i-2])+cost[i];
+            return Math.min(cost[cost.length-1],cost[cost.length-2]);
+        }
+}
