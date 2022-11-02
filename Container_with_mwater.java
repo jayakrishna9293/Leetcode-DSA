44dca82ea535576be217687e18dd93346d7b50aa
jayakrishna9293 <55653899+jayakrishna9293@users.noreply.github.com>
2022-11-02 10:05:00 +0530

    Add files via upload

---
+++ Container_with_mwater.java
@@ -0,0 +1,22 @@
+package math
+
+class Container_with_mwater {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        
+        while (left < right) {
+            int width = right - left;
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * width);
+            
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return maxArea;
+    }
+}
